5472fe54bf04a3282689f89840ed2e79a4080658
JJH <az32111@naver.com>
2019-09-25 20:37:17 +0900

    백업

---
+++ app/src/main/java/com/example/mynewapp/Main2Activity.java
@@ -0,0 +1,43 @@
+package com.example.mynewapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class Main2Activity extends AppCompatActivity {
+    EditText idd, pww;
+    Button button;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        idd = findViewById(R.id.userid);
+        pww = findViewById(R.id.userpw);
+        button = findViewById(R.id.button);
+
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (idd.getText().toString().equals("1234") && pww.getText().toString().equals("3333")) {
+                    Intent intent = new Intent(Main2Activity.this, Main3Activity.class);
+                    startActivity(intent);
+                    finish();
+                }else{
+                    Toast.makeText(Main2Activity.this,"아이디 혹은 비번입력 오류",Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+
+    }
+
+}
